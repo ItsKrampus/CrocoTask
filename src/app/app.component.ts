8b74a371784a898232a182c9814d9edc022cb68a
ItsKrampus <106743990+ItsKrampus@users.noreply.github.com>
2024-07-12 22:34:12 +0400

    added header and sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, SidenavComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'task';
--- src/app/components/users-list/users-item/users-item.component.ts
+++ src/app/components/users-list/users-item/users-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-users-item',
@@ -9,5 +9,4 @@ import {Component, Input} from '@angular/core';
 })
 export class UsersItemComponent {
   @Input() usersData: any;
-  
 }
